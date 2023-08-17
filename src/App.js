e25ab707e8eeab9ad79affcd67aba8e2e38c7320
sejalkataria <katariasejal18@gmail.com>
2023-08-17 12:54:31 +0530

    added navbar design and pagination design from tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import Counter  from './features/counter/Counter';
-import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ProductList></ProductList>
+        <Home></Home>
       </header>
     </div>
   );
