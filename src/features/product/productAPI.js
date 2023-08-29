c635dd195e1ff891a9802a4e6011ecf280727f5f
sejalkataria <katariasejal18@gmail.com>
2023-08-29 15:35:24 +0530

    added pagination code

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,11 +8,10 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter, sort) {
+export function fetchProductsByFilters(filter, sort, pagination) {
   let queryString = ''
   for (let key in filter) {
     const categoryValues = filter[key]
-    console.log("cateee", categoryValues)
     if (categoryValues.length) {
       const lastCategoryValue = categoryValues[categoryValues.length - 1]
       queryString += `${key}=${lastCategoryValue}&`
@@ -23,10 +22,15 @@ export function fetchProductsByFilters(filter, sort) {
     queryString += `${key}=${sort[key]}&`
   }
 
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`
+  }
+
   return new Promise(async (resolve) => {
     const response = await fetch(`http://localhost:8080/products?${queryString}`)
-    const data = response.json()
-    resolve({ data })
+    const data = await response.json()
+    const totalItems = response.headers.get('X-Total-Count')
+    resolve({ data: { products: data, totalItems: totalItems } })
   }
   );
 }
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -4,6 +4,7 @@ import { fetchAllProducts, fetchProductsByFilters } from './productAPI';
 const initialState = {
   products: [],
   status: 'idle',
+  totalItems: 0
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
@@ -16,8 +17,8 @@ export const fetchAllProductsAsync = createAsyncThunk(
 
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
-  async ({ filter, sort }) => {
-    const response = await fetchProductsByFilters(filter, sort);
+  async ({ filter, sort, pagination }) => {
+    const response = await fetchProductsByFilters(filter, sort, pagination);
     return response.data;
   }
 );
@@ -49,13 +50,17 @@ export const productSlice = createSlice({
       })
       .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.products = action.payload;
+        state.products = action.payload.products;
+        state.totalItems = action.payload.totalItems;
       });
   },
 });
 
+
+
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
